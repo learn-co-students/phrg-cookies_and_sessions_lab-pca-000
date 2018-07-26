465e9dcc31326830f8e964edb073c1aa6d8fa1c7
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-26 10:41:52 -0400

    cookies and session practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+protected
+
+  def cart
+    session[:cart] = Array(session[:cart])
+  end
+  helper_method :cart
 end
